4412afe2718f2d418324914a1a3bf4918f6dd9e1
DinandV <dinand.veldman@sommite.nl>
2022-11-12 16:53:37 +0100

    Added DateAndTime.AsYYYYMMDD

---
+++ NetExtensions.NetCore/DateAndTime/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExtensions.NetCore.DateAndTime
+{
+    public static class Extensions
+    {
+
+        public static int AsYYYYMMDD(this DateTime dt)
+        {
+            var y = dt.Year.ToString(); var m = dt.Month.ToString(); var d = dt.Day.ToString();
+            var ymd = $"{y}{m.PadLeft(2, '0')}{d.PadLeft(2, '0')}";
+            int result = 0;
+            if (!int.TryParse(ymd, out result)) throw new ConversionException("Error converting date to integer");
+            return result;
+        }
+
+    }
+
+
+}
+
+namespace NetExtensions
+{
+    public class ConversionException : Exception
+    {
+      public ConversionException(string message):base(message)
+        {
+            
+        }
+    }
+}
