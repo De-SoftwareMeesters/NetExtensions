de574a47c5e1ea3b3017c04810222dce9ea2a588
DinandV <dinand.veldman@sommite.nl>
2022-11-17 16:35:32 +0100

    Added ToYYYYMMDD and ToDateTime

--- NetExtensions.NetCore/DateAndTime/Extensions.cs
+++ NetExtensions.NetCore/DateAndTime/Extensions.cs
@@ -11,14 +11,41 @@ namespace NetExtensions.NetCore.DateAndTime
 {
     public static class Extensions
     {
-
-        public static int AsYYYYMMDD(this DateTime dt)
+        /// <summary>
+        /// This method returns the YYYYMMDD equivalent of the date you put in it.
+        /// It returns an integer
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        /// <exception cref="ConversionException"></exception>
+        public static int ToYYYYMMDD(this DateTime dt)
         {
             var y = dt.Year.ToString(); var m = dt.Month.ToString(); var d = dt.Day.ToString();
             var ymd = $"{y}{m.PadLeft(2, '0')}{d.PadLeft(2, '0')}";
             int result = 0;
-            if (!int.TryParse(ymd, out result)) throw new ConversionException("Error converting date to integer");
+            if (!int.TryParse(ymd, out result)) throw new ConversionException($"Error converting date {dt.ToShortDateString()} to integer");
             return result;
+        }
+
+        /// <summary>
+        /// This method tries to convert a string to a DateTime.
+        /// I'm dutch, so I expect you to pass in a string in the Dutch date format (DD-MM-YYYY)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        /// <exception cref="ConversionException"></exception>
+        public static DateTime ToDateTime(this string s)
+        {
+            try
+            {
+                int d, m, j;
+                int.TryParse(s.Substring(0, 2), out d);
+                int.TryParse(s.Substring(3, 2), out m);
+                int.TryParse(s.Substring(6, 4), out j);
+
+                return new DateTime(j, m, d);
+            }
+            catch { throw new ConversionException($"Error converting string {s} to DateTime"); }
         }
 
     }
@@ -30,9 +57,9 @@ namespace NetExtensions
 {
     public class ConversionException : Exception
     {
-      public ConversionException(string message):base(message)
+        public ConversionException(string message) : base(message)
         {
-            
+
         }
     }
 }
