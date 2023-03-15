1d862d7ec9153782bcb180e8a27a1613bbe07459
DinandV <dinand.veldman@sommite.nl>
2023-03-15 13:11:53 +0100

    Added types

---
+++ NetExtensions.NetCore/Types/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExtensions.NetCore.Types
+{
+    public static class Extensions
+    {
+        public static bool IsOnlyNumbers(this string s)
+        {
+            return s.All(char.IsNumber);
+        }
+
+        public static string NullThenEmpty(this string s)
+        {
+            return s ?? ""; 
+        }
+    }
+}
